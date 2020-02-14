670b945b1adb1219124f84d78500bc71d846fe00
Chris Ertel <>
2020-02-14 01:20:35 -0600

    Fix broken images, start stubbing in ingest + db.

--- ingest/src/app.py
+++ ingest/src/app.py
@@ -1,1 +1,29 @@
-print("Hello, world!")
+import psycopg2
+from environs import Env
+
+env = Env()
+env.read_env()
+
+DEBUG = env.bool("DEBUG")
+PG_HOST = env("PG_HOST")
+PG_USER = env("PG_USER")
+PG_PASSWORD = env("PG_PASSWORD")
+PG_DB = env("PG_DB")
+PG_PORT = env.int("PG_PORT")
+
+PG_URL = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}"
+
+print(f"Connecting to {PG_URL}...")
+
+
+try:
+    conn = psycopg2.connect(PG_URL)
+    c = conn.cursor()
+    c.execute("select 'hello, world!'")
+    rows = c.fetchall()
+    for r in rows:
+        print(r)
+
+except Exception as e:
+    print("I am unable to connect to the database:")
+    print(e)
