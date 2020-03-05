fc584d16e56528d009b86e77dd2c3df2c862a0a9
Chris Ertel <>
2020-03-05 00:38:20 -0600

    Add debug/hot-reloading to ingest.

--- ingest/run.py
+++ ingest/run.py
@@ -1,8 +1,13 @@
 from plugin_manager import PluginManager
 from app import app
-from config import LISTEN_PORT
+from config import LISTEN_PORT, DEBUG
 
 if __name__ == "__main__":
     pm = PluginManager()
     pm.load_plugins()
-    app.run(host="0.0.0.0", port=LISTEN_PORT)
+    if DEBUG:
+        app.jinja_env.auto_reload = True
+        app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+    app.run(host="0.0.0.0", debug=DEBUG, port=LISTEN_PORT)
+
